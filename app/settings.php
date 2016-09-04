50d1d6e597ca2a9011c2f6b80799185328902956
Farhad <me.mfhossen@gmail.com>
2016-09-04 14:39:04 +0600

    remove twig cache

--- app/settings.php
+++ app/settings.php
@@ -9,7 +9,7 @@ return [
         'view' => [
             'template_path' => __DIR__ . '/templates',
             'twig' => [
-                'cache' => __DIR__ . '/../cache/twig',
+                //'cache' => __DIR__ . '/../cache/twig',
                 'debug' => true,
                 'auto_reload' => true,
             ],
