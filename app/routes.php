26c4d02e475fd03a28351c3158817bdd106d063a
shanta <mimetyshanta@gmail.com>
2016-09-04 15:15:52 +0600

    Task route  is created

--- app/routes.php
+++ app/routes.php
@@ -6,8 +6,15 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->get('/', function (Request $request, Response $response) {
 
     //var_dump(R::dispense('tasks')); die();
-    return $this->view->render($response, 'layout.twig');
+    //return $this->view->render($response, 'layout.twig');
     return $this->view->render($response, 'welcome.twig');
+});
+
+$app->get('/task', function (Request $request, Response $response) {
+
+    //var_dump(R::dispense('tasks')); die();
+    //return $this->view->render($response, 'layout.twig');
+    return $this->view->render($response, 'task.twig');
 });
 
 
