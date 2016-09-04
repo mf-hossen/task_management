390955e46c840eb17d05cc9ac756ce749cfa36c6
shanta <mimetyshanta@gmail.com>
2016-09-04 17:17:19 +0600

    user and task is now separate

--- app/routes.php
+++ app/routes.php
@@ -7,26 +7,6 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require "routes/task.php";
 require "routes/user.php";
 
-$app->get('/', function (Request $request, Response $response) {
-
-    //var_dump(R::dispense('tasks')); die();
-   // return $this->view->render($response, 'layout.twig');
-    return $this->view->render($response, 'welcome.twig');
-});
-
-$app->get('/task', function (Request $request, Response $response) {
-    return $this->view->render($response, 'task.twig');
-});
-
-$app->post('/insert', function (Request $request, Response $response) {
-    $data = $request->getParsedBody();
-    //var_dump($data);die();
-    $mapper = new \App\TaskMapper($this->db);
-    $task=$mapper->addTask($data);
-    var_dump($task);die();
-
-
-});
 
 
 
--- app/routes/task.php
+++ app/routes/task.php
@@ -4,9 +4,17 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 
-$app->get('/task', function (Request $request, Response $response) {
 
-    //var_dump(R::dispense('tasks')); die();
-    //return $this->view->render($response, 'layout.twig');
+$app->get('/task', function (Request $request, Response $response) {
     return $this->view->render($response, 'task.twig');
+});
+
+$app->post('/insert', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    //var_dump($data);die();
+    $mapper = new \App\TaskMapper($this->db);
+    $task=$mapper->addTask($data);
+    var_dump($task);die();
+
+
 });
