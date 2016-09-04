c975556e2cc05d21c601e9f6f3a2700c56011e5b
Farhad <me.mfhossen@gmail.com>
2016-09-04 13:02:40 +0600

    sb admin template  add

--- app/src/StudentMapper.php
+++ app/src/StudentMapper.php
@@ -5,9 +5,5 @@ namespace App;
 
 class StudentMapper extends Mapper
 {
-    public function getStudent() {
-        $sql = "Select * from student";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
-    }
+
 }
