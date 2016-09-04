3bbfeb0070a20eea0efbf9f95a2e2c88e7131c65
Farhad <me.mfhossen@gmail.com>
2016-09-04 18:34:44 +0600

    new column add

--- log.sql
+++ log.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `tasks` CHANGE `status` `status` TINYINT(4) NULL DEFAULT NULL COMMENT '1=edit, 2=redesign,3=new ';
+
+/* new column username add*/
+
+ALTER TABLE `users` ADD `username` INT(100) NOT NULL AFTER `id`;
