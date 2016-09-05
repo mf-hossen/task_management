3329000ad170ee0ec27cbf2f461ffa0523e46fa0
shanta <mimetyshanta@gmail.com>
2016-09-05 14:06:18 +0600

    insert new task except user_id(session), submission_date field changed

--- log.sql
+++ log.sql
@@ -3,3 +3,8 @@ ALTER TABLE `tasks` CHANGE `status` `status` TINYINT(4) NULL DEFAULT NULL COMMEN
 /* new column username add*/
 
 ALTER TABLE `users` ADD `username` INT(100) NOT NULL AFTER `id`;
+
+-- shanta
+ALTER TABLE `users` CHANGE `username` `username` VARCHAR(100) NOT NULL;
+--shanta
+ALTER TABLE `tasks` CHANGE `created_at` `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
