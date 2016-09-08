c1e0719538b027b3e1ee561b35594a06951ef7cc
shanta <mimetyshanta@gmail.com>
2016-09-08 13:49:17 +0600

    change task table field with task_type

--- db/log.sql
+++ db/log.sql
@@ -9,3 +9,4 @@ ALTER TABLE `users` CHANGE `username` `username` VARCHAR(100) NOT NULL;
 --shanta
 ALTER TABLE `tasks` CHANGE `created_at` `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE `tasks` CHANGE `status` `status` VARCHAR(10) NOT NULL COMMENT 'edit, redesign,new ';
+ALTER TABLE `tasks` CHANGE `status` `task_type` TINYINT(4) NOT NULL COMMENT '1=edit, 2=redesign,3=new';
