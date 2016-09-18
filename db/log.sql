3fd8fb3c14378667b13ac5c5e4da5b9ce610a3c5
Emon <emonmr@gmail.com>
2016-09-18 17:30:40 +0600

    Admin tasklist is redesigned

--- db/log.sql
+++ db/log.sql
@@ -12,4 +12,4 @@ ALTER TABLE `tasks` CHANGE `status` `status` VARCHAR(10) NOT NULL COMMENT 'edit,
 ALTER TABLE `tasks` CHANGE `status` `task_type` TINYINT(4) NOT NULL COMMENT '1=edit, 2=redesign,3=new';
 
 -- Emon 18 9 2016
-ALTER TABLE `tasks` CHANGE `status` `status` TINYINT(4) NOT NULL DEFAULT '3' COMMENT '1=Complete, 2=Incomplete, 3=Pending; 4=Invalid; 5=Done';
+ALTER TABLE `tasks` CHANGE `status` `status` TINYINT(4) NOT NULL DEFAULT '3' COMMENT '1=Complete, 2=Incomplete, 3=Pending; 4=Done; 5=Invalid';
