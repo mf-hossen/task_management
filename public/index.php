f3ffcac3afce3332977b8f4a4018cfdb551f63bb
sharminex81 <mimetyshanta@gmail.com>
2016-12-08 17:26:07 +0600

    remove the junk code

--- public/index.php
+++ public/index.php
@@ -11,13 +11,6 @@ require __DIR__ . '/../vendor/autoload.php';
 // Session timeout
 session_start();
 
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 36000)) {
-    // last request was more than 30 minutes ago
-    session_unset();     // unset $_SESSION variable for the run-time
-    session_destroy();   // destroy session data in storage
-}
-$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
-
 // DB config
 require __DIR__ . '/../db_config.php';
 
